f78925b2da59d1b29138f6ee98316b607310c18e
stone <877627290@qq.com>
2025-02-20 16:01:49 +0800

    update(uemo-editor-element): 新增 EditorUnit 组件并更新路由配置

---
+++ uebuilder-plugins/uemo-editor-element/demo/router/EditorUnit.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="$style['editor-page']">
+        <div :class="$style['test-group']" class="grid grid-cols-2">
+            <TestComponent />
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import TestComponent from "~/demo/components/TestIcon.vue";
+</script>
+<style lang="scss" module>
+.test-group {
+    width: 100%;
+    padding: 20px;
+
+    gap: 70px 20px;
+    .row {
+        grid-area: auto / auto / auto / span 2;
+    }
+}
+</style>
--- uebuilder-plugins/uemo-editor-element/demo/router/index.ts
+++ uebuilder-plugins/uemo-editor-element/demo/router/index.ts
@@ -1,17 +1,18 @@
 /*
  * @Description: 路由入口文件
  * @Author: F-Stone
- * @LastEditTime: 2024-11-27 14:20:25
+ * @LastEditTime: 2025-02-20 16:01:00
  */
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import CurrentView from "./Current.vue";
+import EditorUnitView from "./EditorUnit.vue";
 
 export default createRouter({
     history: createMemoryHistory(),
     routes: [
         { path: "/", component: CurrentView },
-        { path: "/editor-unit", component: CurrentView },
+        { path: "/editor-unit", component: EditorUnitView },
         { path: "/editor-panel", component: CurrentView },
     ],
 });
