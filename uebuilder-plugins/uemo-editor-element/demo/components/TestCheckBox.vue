70742f56295848ad37ceda44d736a82c528f2b4b
stone <877627290@qq.com>
2025-02-24 00:49:59 +0800

    update(uemo-editor-element): 添加单选框组件及相关功能

--- uebuilder-plugins/uemo-editor-element/demo/components/TestCheckBox.vue
+++ uebuilder-plugins/uemo-editor-element/demo/components/TestCheckBox.vue
@@ -1,11 +1,17 @@
 <!--
  * @Description: 测试单选框
  * @Author: F-Stone
- * @LastEditTime: 2025-02-23 23:52:56
+ * @LastEditTime: 2025-02-24 00:37:14
 -->
 <template>
-    <TestArea :class="$style['test-area']" :value="testValue" title="测试单选框">
-        <UeElCheckBox v-bind="testValue">
+    <TestArea
+        :class="$style['test-area']"
+        :testValue="testValue"
+        :testValueList="testValueList"
+        v-model:testValueSelect="testValueSelect"
+        title="测试单选框"
+    >
+        <UeElCheckBox v-bind="testValue" v-model:value="testValue.value">
             <!--  -->
         </UeElCheckBox>
     </TestArea>
@@ -15,7 +21,9 @@ import TestArea from "~/demo/components/TestArea.vue";
 
 // 测试数据
 const testValueSelect = ref<number>(0);
-const testValueList: (UE_EL_COMPONENT.UeElCheckBoxProps & { value?: any })[] = [];
+const testValueList: (UE_EL_COMPONENT.UeElCheckBoxProps & { value?: any })[] = [
+    { value: false, text: "测试单选框", label: "测试单选框" },
+];
 const testValue = ref(testValueList[testValueSelect.value]);
 
 watch(testValueSelect, (newValue) => {
--- uebuilder-plugins/uemo-editor-element/packages/check-box/Main.vue
+++ uebuilder-plugins/uemo-editor-element/packages/check-box/Main.vue
@@ -1,11 +1,22 @@
 <!--
  * @Description: 单选框
  * @Author: F-Stone
- * @LastEditTime: 2025-02-23 23:56:20
+ * @LastEditTime: 2025-02-24 00:23:48
 -->
 <template>
-    <div :class="$style['check-box']">
-        <!--  -->
+    <div
+        :class="$style['check-box']"
+        class="cursor-pointer flex items-center"
+        :data-active="value ? '' : undefined"
+        :data-disable="disable ? '' : undefined"
+        @click="trigger"
+    >
+        <div :class="$style['box']" class="flex justify-center items-center">
+            <div :class="$style['box-inner']">
+                <UeElIcon v-if="value" :size="12" :class="$style['select-icon']" name="icon-duigou" />
+            </div>
+        </div>
+        <span v-ue-el-label="label" :class="$style['text']">{{ text }}</span>
     </div>
 </template>
 <script lang="ts" setup>
@@ -14,10 +25,55 @@ import type { UeElCheckBoxBaseProps } from "./index";
 interface Props extends UeElCheckBoxBaseProps {}
 
 defineOptions({ name: "UeElCheckBox" });
-const _prop = defineProps<Props>();
+
+const prop = defineProps<Props>();
+const valueRef = defineModel("value", { type: Boolean });
+
+const trigger = () => {
+    if (prop.disable) return;
+    valueRef.value = !valueRef.value;
+};
 </script>
 <style lang="scss" module>
 .check-box {
-    //
+    font-size: 12px;
+    line-height: 28px;
+
+    cursor: pointer;
+
+    color: color(var(--ue-font-color));
+    &[data-disable] {
+        cursor: not-allowed;
+
+        opacity: 0.5;
+    }
+    &[data-active] {
+        color: color(var(--ue-font-color--deeper));
+        .box-inner {
+            border-color: color(var(--ue-font-color--deeper));
+        }
+        .select-icon {
+            color: #fff;
+            background-color: color(var(--ue-font-color--deeper));
+        }
+    }
+    &:hover {
+        .tip {
+            opacity: 1;
+        }
+    }
+    .box {
+        @include square(28px);
+    }
+    .box-inner {
+        @include square(14px);
+        position: relative;
+        top: -1px;
+
+        overflow: hidden;
+
+        border: 1px solid color(var(--ue-border-color));
+        border-radius: var(--ue-border-radius--lv1);
+    }
 }
 </style>
