984d8dddc88586eaa80ba89aef567dd29b42f9e9
stone <877627290@qq.com>
2025-03-01 15:56:43 +0800

    update(uemo-editor-element): 添加语言选择器及国际化支持

--- uebuilder-plugins/uemo-editor-element/demo/pages/index/index.vue
+++ uebuilder-plugins/uemo-editor-element/demo/pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div :class="$style['nav-bar']" class="flex justify-center">
+    <div :class="$style['nav-bar']" class="flex justify-center items-center">
+        <UeElSelect v-model:value="currentLang" title="测试选择器" :options="langOptions" />
         <RouterLink v-for="(item, index) in linkList" :key="index" exact :class="$style['link']" :to="item.path">
             {{ item.text }}
         </RouterLink>
@@ -15,6 +16,21 @@ const linkList = ref([
     { path: "/editor-resource/panel", text: "资源组件" },
     { path: "/editor-lib-panel", text: "库组件" },
 ]);
+
+const { locale } = useI18n();
+const langOptions = ref([
+    { value: "en", text: "English" },
+    { value: "zh-cn", text: "中文" },
+]);
+const currentLang = computed({
+    get() {
+        return locale.value;
+    },
+    set(v) {
+        locale.value = v;
+        localStorage.setItem("lang", currentLang.value);
+    },
+});
 </script>
 <style lang="scss" module>
 .nav-bar {
