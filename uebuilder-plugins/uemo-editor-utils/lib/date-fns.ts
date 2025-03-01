52b2008582eb5f601a73ab590b1ad6d779881d83
stone <877627290@qq.com>
2025-03-01 18:47:28 +0800

    update(uemo-editor-utils): 添加 date-fns 库及相关日期处理工具

---
+++ uebuilder-plugins/uemo-editor-utils/lib/date-fns.ts
@@ -0,0 +1,37 @@
+import { add } from "date-fns/add";
+import { sub } from "date-fns/sub";
+import { format } from "date-fns/format";
+import { getDay } from "date-fns/getDay";
+import { getDate } from "date-fns/getDate";
+import { getMonth } from "date-fns/getMonth";
+import { getYear } from "date-fns/getYear";
+import { isAfter } from "date-fns/isAfter";
+import { isBefore } from "date-fns/isBefore";
+import { isSameDay } from "date-fns/isSameDay";
+import { endOfMonth } from "date-fns/endOfMonth";
+import { startOfMonth } from "date-fns/startOfMonth";
+import { eachDayOfInterval } from "date-fns/eachDayOfInterval";
+import { startOfDay } from "date-fns/startOfDay";
+import { endOfDay } from "date-fns/endOfDay";
+import { isSameYear } from "date-fns/isSameYear";
+import { isSameMonth } from "date-fns/isSameMonth";
+
+export {
+    add,
+    sub,
+    format,
+    getDay,
+    getDate,
+    getMonth,
+    getYear,
+    isAfter,
+    isBefore,
+    isSameDay,
+    endOfMonth,
+    startOfMonth,
+    eachDayOfInterval,
+    startOfDay,
+    endOfDay,
+    isSameYear,
+    isSameMonth,
+};
