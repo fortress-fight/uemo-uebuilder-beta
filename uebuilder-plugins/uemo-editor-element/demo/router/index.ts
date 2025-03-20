f534cb3dc20cc5face730a4481cfba833cb83086
stone <877627290@qq.com>
2025-03-21 01:53:35 +0800

    update(uemo-editor-element): 添加属性面板组件路由及相关页面

--- uebuilder-plugins/uemo-editor-element/demo/router/index.ts
+++ uebuilder-plugins/uemo-editor-element/demo/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Description: 路由入口文件
  * @Author: F-Stone
- * @LastEditTime: 2025-03-07 13:50:44
+ * @LastEditTime: 2025-03-21 00:15:38
  */
 import { createMemoryHistory, createRouter } from "vue-router";
 
@@ -11,6 +11,7 @@ import AttrEditorUnitView from "./AttrEditorUnit.vue";
 import EditorPanelView from "./EditorPanel.vue";
 import EditorFormUnitView from "./EditorFormUnit.vue";
 import ResourcePanelView from "./ResourcePanel.vue";
+import AttrEditorPanelView from "./AttrEditorPanel.vue";
 
 export default createRouter({
     history: createMemoryHistory(),
@@ -21,5 +22,6 @@ export default createRouter({
         { path: "/editor-panel", component: EditorPanelView },
         { path: "/editor-form", component: EditorFormUnitView },
         { path: "/editor-resource-panel", component: ResourcePanelView },
+        { path: "/editor-attr-editor-panel", component: AttrEditorPanelView },
     ],
 });
