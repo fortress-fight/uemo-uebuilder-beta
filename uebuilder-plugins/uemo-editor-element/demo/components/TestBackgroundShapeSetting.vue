662642b6c0cd2687dc332d7bb13e22a157fb6bfd
stone <877627290@qq.com>
2025-03-23 01:22:31 +0800

    update(uemo-editor-element): 更新背景图形控制器组件，修复双向绑定并简化数据处理逻辑

--- uebuilder-plugins/uemo-editor-element/demo/components/TestBackgroundShapeSetting.vue
+++ uebuilder-plugins/uemo-editor-element/demo/components/TestBackgroundShapeSetting.vue
@@ -1,7 +1,7 @@
 <!--
  * @Description: 测试背景图形控制器组件
  * @Author: F-Stone
- * @LastEditTime: 2025-03-21 11:38:59
+ * @LastEditTime: 2025-03-23 01:21:41
 -->
 <template>
     <TestArea
@@ -11,7 +11,7 @@
         v-model:testValueSelect="testValueSelect"
         title="测试背景图形控制器组件"
     >
-        <UeElBackgroundShapeSetting v-bind="testValue" @update:value="change">
+        <UeElBackgroundShapeSetting v-bind="testValue" v-model:value="testValue.value">
             <!--  -->
         </UeElBackgroundShapeSetting>
     </TestArea>
@@ -22,19 +22,13 @@ import TestArea from "~/demo/components/TestArea.vue";
 // 测试数据
 const testValueSelect = ref<number>(0);
 const testValueList: (UE_EL_COMPONENT.UeElBackgroundShapeSettingProps & { testOptionTitle?: string; value?: any })[] = [
-    {
-        value: {},
-    },
+    { value: { name: "shape-bg-2", mirroring: false } },
 ];
 const testValue = ref(testValueList[testValueSelect.value]);
 
 watch(testValueSelect, (newValue) => {
     testValue.value = testValueList[newValue];
 });
-
-function change(value: any) {
-    console.log("change---value", value);
-}
 </script>
 <style lang="scss" module>
 .test-area {
