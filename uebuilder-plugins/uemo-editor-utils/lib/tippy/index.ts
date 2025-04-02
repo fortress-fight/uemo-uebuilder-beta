da0c6129ba42f36c66bff72023239d1895eede77
stone <877627290@qq.com>
2025-04-02 23:15:46 +0800

    update(uemo-editor-element): 集成 vue-tippy 插件，重构标签指令，移除冗余代码，新增样式文件

---
+++ uebuilder-plugins/uemo-editor-utils/lib/tippy/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: tippy 插件
+ * @Author: F-Stone
+ * @LastEditTime: 2025-04-02 23:06:43
+ */
+export type { Instance, Props, Placement } from "tippy.js";
+
+import "tippy.js/dist/tippy.css";
+import "./style.scss";
+
+export * from "vue-tippy";
