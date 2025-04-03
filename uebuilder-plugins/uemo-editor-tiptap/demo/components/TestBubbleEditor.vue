5d3a43e86b2bd38652c64a55d70459d472dccf91
stone <877627290@qq.com>
2025-04-04 06:14:34 +0800

    update(uemo-editor-tiptap): 修改测试气泡模式编辑器，更新数据绑定和处理函数以支持实时预览

--- uebuilder-plugins/uemo-editor-tiptap/demo/components/TestBubbleEditor.vue
+++ uebuilder-plugins/uemo-editor-tiptap/demo/components/TestBubbleEditor.vue
@@ -1,22 +1,24 @@
 <!--
  * @Description: 测试气泡模式编辑器
  * @Author: F-Stone
- * @LastEditTime: 2025-04-01 00:48:09
+ * @LastEditTime: 2025-04-04 04:21:28
 -->
 <template>
     <TestArea
         :class="$style['test-area']"
-        :testValue="testValue"
+        :testValue="previewData"
         :testValueList="testValueList"
         v-model:testValueSelect="testValueSelect"
         title="测试气泡模式编辑器"
     >
-        <UeTiptapBubbleEditor v-bind="testValue">
+        <UeTiptapBubbleEditor v-bind="testValue" @update="handleUpdate" @ready="handleUpdate">
             <!--  -->
         </UeTiptapBubbleEditor>
     </TestArea>
 </template>
 <script lang="ts" setup>
+import type { Editor } from "@tiptap/vue-3";
+
 import TestArea from "~/demo/components/TestArea.vue";
 import { testTextContent } from "~/demo/data/test-content";
 // 测试数据
@@ -32,6 +34,20 @@ const testValue = ref(testValueList[testValueSelect.value]);
 watch(testValueSelect, (newValue) => {
     testValue.value = testValueList[newValue];
 });
+
+const previewData = ref<{
+    html: string;
+    json: ReturnType<Editor["getJSON"]>;
+}>({
+    html: "",
+    json: {},
+});
+
+function handleUpdate(editor: Editor) {
+    if (!editor) return;
+    previewData.value.html = editor.getHTML();
+    previewData.value.json = editor.getJSON();
+}
 </script>
 <style lang="scss" module>
 .test-area {
