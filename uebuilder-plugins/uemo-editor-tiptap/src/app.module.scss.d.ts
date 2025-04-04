5de01565d3428262e9b3366ec65d5433b3cd8888
stone <877627290@qq.com>
2025-04-04 17:19:02 +0800

    update(uemo-editor-tiptap): 重构样式文件，新增文本装饰和编辑器样式，优化页面编辑器的字体和布局设置

--- uebuilder-plugins/uemo-editor-tiptap/src/app.module.scss.d.ts
+++ uebuilder-plugins/uemo-editor-tiptap/src/app.module.scss.d.ts
@@ -1,6 +1,9 @@
 declare const styles: {
     readonly "drop-cursor": string;
     readonly "has-focus": string;
+    readonly "page-editor": string;
+    readonly "text-decoration-svg": string;
+    readonly "text-decoration-svg-box": string;
     readonly "ue-richtext-editor": string;
 };
 export = styles;
