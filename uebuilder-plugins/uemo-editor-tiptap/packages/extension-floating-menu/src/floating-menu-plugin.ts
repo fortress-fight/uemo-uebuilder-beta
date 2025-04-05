02a30f71cd5566345dcca476df5c71c1720caade
stone <877627290@qq.com>
2025-04-06 01:49:04 +0800

    update(uemo-editor-tiptap): 重构浮动菜单插件，移除不必要的 DOM 元素和 tippy.js 配置，新增控制菜单显示的函数，优化菜单显示逻辑

--- uebuilder-plugins/uemo-editor-tiptap/packages/extension-floating-menu/src/floating-menu-plugin.ts
+++ uebuilder-plugins/uemo-editor-tiptap/packages/extension-floating-menu/src/floating-menu-plugin.ts
@@ -1,11 +1,9 @@
 import type { EditorView } from "@tiptap/pm/view";
-import type { Instance, Props } from "@stone/uemo-editor-utils/lib/tippy";
 import type { Node as ProseMirrorNode } from "@tiptap/pm/model";
+import type { Props } from "@stone/uemo-editor-utils/lib/tippy";
 
 import { Editor, posToDOMRect, getText, getTextSerializersFromSchema } from "@tiptap/core";
 import { EditorState, Plugin, PluginKey } from "@tiptap/pm/state";
-import { tippy } from "@stone/uemo-editor-utils/lib/tippy";
-import $ from "@stone/uemo-editor-utils/lib/jquery";
 
 /**
  * 浮动菜单插件的配置属性接口
@@ -23,17 +21,6 @@ export interface FloatingMenuPluginProps {
     editor: Editor;
 
     /**
-     * 包含菜单内容的 DOM 元素
-     */
-    element: HTMLElement;
-
-    /**
-     * tippy.js 的配置选项
-     * @see https://atomiks.github.io/tippyjs/v6/all-props/
-     */
-    tippyOptions?: Partial<Props>;
-
-    /**
      * 决定是否显示菜单的判断函数
      * @param {Object} props - 包含编辑器状态的属性对象
      * @returns {boolean} 是否显示菜单
@@ -41,6 +28,11 @@ export interface FloatingMenuPluginProps {
     shouldShow?:
         | ((props: { editor: Editor; view: EditorView; state: EditorState; oldState?: EditorState }) => boolean)
         | null;
+
+    /**
+     * 控制菜单显示的函数
+     */
+    controller?: (type: "show" | "update" | "hide", tippy?: Partial<Props>) => void;
 }
 
 export type FloatingMenuViewProps = FloatingMenuPluginProps & {
@@ -57,15 +49,11 @@ export type FloatingMenuViewProps = FloatingMenuPluginProps & {
 export class FloatingMenuView {
     public editor: Editor;
 
-    public element: HTMLElement;
-
     public view: EditorView;
 
     public preventHide = false;
 
-    public tippy: Instance | undefined;
-
-    public tippyOptions?: Partial<Props>;
+    public controller?: FloatingMenuPluginProps["controller"];
 
     /**
      * 获取节点的文本内容
@@ -103,12 +91,10 @@ export class FloatingMenuView {
         return true;
     };
 
-    constructor({ editor, element, view, tippyOptions = {}, shouldShow }: FloatingMenuViewProps) {
+    constructor({ editor, view, shouldShow, controller }: FloatingMenuViewProps) {
         this.editor = editor;
-        this.element = element;
         this.view = view;
-
-        if (!this.element) return;
+        this.controller = controller;
 
         // 如果传入了自定义的 shouldShow 方法,则覆盖默认的显示逻辑
         if (shouldShow) {
@@ -116,22 +102,9 @@ export class FloatingMenuView {
         }
 
         // 添加鼠标按下事件监听器,用于阻止菜单隐藏
-        this.element.addEventListener("mousedown", this.mousedownHandler, { capture: true });
-
         this.editor.on("focus", this.focusHandler);
         this.editor.on("blur", this.blurHandler);
-        this.tippyOptions = tippyOptions;
-
-        // 从当前父节点中移除菜单元素
-        this.element.remove();
-
-        // 设置菜单元素可见
-        this.element.style.visibility = "visible";
     }
-
-    mousedownHandler = () => {
-        this.preventHide = true;
-    };
 
     focusHandler = () => {
         // we use `setTimeout` to make sure `selection` is already updated
@@ -139,16 +112,6 @@ export class FloatingMenuView {
     };
 
     blurHandler = ({ event }: { event: FocusEvent }) => {
-        if (this.preventHide) {
-            this.preventHide = false;
-
-            return;
-        }
-
-        if (event?.relatedTarget && this.element.parentNode?.contains(event.relatedTarget as Node)) {
-            return;
-        }
-
         if (event?.relatedTarget === this.editor.view.dom) {
             return;
         }
@@ -160,36 +123,17 @@ export class FloatingMenuView {
         this.blurHandler({ event });
     };
 
-    /**
-     * 创建 tippy 工具提示实例
-     * @private
-     */
-    createTooltip() {
-        const { element: editorElement } = this.editor.options;
-        const editorIsAttached = !!editorElement.parentElement;
-
-        if (this.tippy || !editorIsAttached) {
-            return;
-        }
-
-        this.tippy = tippy(editorElement, {
+    get tippyOptions(): Partial<Props> {
+        return {
             duration: [200, 300],
             zIndex: 900,
             getReferenceClientRect: null,
-            content: this.element,
             interactive: true,
             trigger: "manual",
             placement: "right",
             hideOnClick: "toggle",
             theme: "tip-tap",
-            ...this.tippyOptions,
-        });
-        $(this.element).data("_tippy", this.tippy);
-
-        // maybe we have to hide tippy on its own blur event as well
-        if (this.tippy.popper.firstChild) {
-            (this.tippy.popper.firstChild as HTMLElement).addEventListener("blur", this.tippyBlurHandler);
-        }
+        };
     }
 
     /**
@@ -220,33 +164,26 @@ export class FloatingMenuView {
             return;
         }
 
-        this.createTooltip();
-
         if (this.editor.isFocused) {
-            this.tippy?.setProps({
-                getReferenceClientRect:
-                    this.tippyOptions?.getReferenceClientRect || (() => posToDOMRect(view, from, to)),
-            });
-
-            this.show();
+            this.show(
+                Object.assign(this.tippyOptions, {
+                    getReferenceClientRect: () => posToDOMRect(view, from, to),
+                })
+            );
         }
     }
 
-    show() {
-        this.tippy?.show();
+    show(tippy: Partial<Props>) {
+        this.controller?.("show", tippy);
     }
 
     hide() {
-        this.tippy?.hide();
+        this.controller?.("hide");
     }
 
     destroy() {
-        if (!this.element) return;
-        if (this.tippy?.popper.firstChild) {
-            (this.tippy.popper.firstChild as HTMLElement).removeEventListener("blur", this.tippyBlurHandler);
-        }
-        this.tippy?.destroy();
-        this.element.removeEventListener("mousedown", this.mousedownHandler, { capture: true });
+        this.controller?.("hide");
+
         this.editor.off("focus", this.focusHandler);
         this.editor.off("blur", this.blurHandler);
     }
--- uebuilder-plugins/uemo-editor-tiptap/packages/extension-floating-menu/src/floating-menu.ts
+++ uebuilder-plugins/uemo-editor-tiptap/packages/extension-floating-menu/src/floating-menu.ts
@@ -4,14 +4,7 @@ import { Extension } from "@tiptap/core";
 
 import { FloatingMenuPlugin } from "./floating-menu-plugin";
 
-export type FloatingMenuOptions = Omit<FloatingMenuPluginProps, "editor" | "element"> & {
-    /**
-     * The DOM element that contains your menu.
-     * @type {HTMLElement}
-     * @default null
-     */
-    element: HTMLElement | null;
-};
+export type FloatingMenuOptions = Omit<FloatingMenuPluginProps, "editor">;
 
 /**
  * This extension allows you to create a floating menu.
@@ -22,24 +15,16 @@ export const FloatingMenu = Extension.create<FloatingMenuOptions>({
 
     addOptions() {
         return {
-            element: null,
-            tippyOptions: {},
             pluginKey: "floatingMenu",
             shouldShow: null,
         };
     },
 
     addProseMirrorPlugins() {
-        if (!this.options.element) {
-            return [];
-        }
-
         return [
             FloatingMenuPlugin({
                 pluginKey: this.options.pluginKey,
                 editor: this.editor,
-                element: this.options.element,
-                tippyOptions: this.options.tippyOptions,
                 shouldShow: this.options.shouldShow,
             }),
         ];
