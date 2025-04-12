52e341858297531a3591bd3284fa1528d817b75d
stone <877627290@qq.com>
2025-04-12 16:40:37 +0800

    update(uemo-editor-tiptap): 增强文本装饰功能，优化选区处理逻辑以支持链接选区的编辑

--- uebuilder-plugins/uemo-editor-tiptap/packages/extension-text-decoration/src/text-decoration.ts
+++ uebuilder-plugins/uemo-editor-tiptap/packages/extension-text-decoration/src/text-decoration.ts
@@ -2,7 +2,8 @@ import type { Attribute } from "@tiptap/core";
 import type { TextDecorationAttrs } from "./index";
 
 import "@tiptap/extension-text-style";
-import { Mark, getMarkAttributes } from "@tiptap/core";
+import { TextSelection } from "@tiptap/pm/state";
+import { Mark, getMarkAttributes, getMarkRange } from "@tiptap/core";
 
 import $pageStyle from "../../../src/app.module.scss";
 import decorationSvgData from "../utils/decoration-svg-data";
@@ -149,6 +150,21 @@ export const TextDecoration = Mark.create<TextDecorationOptions>({
                 ({ editor, state, commands }) => {
                     const textDecorationAttr = getMarkAttributes(state, this.name) || {};
                     const selection = editor.state.selection;
+
+                    const { $from, $to } = selection;
+                    const startMarkRange = getMarkRange($from, editor.state.schema.marks.textDecoration);
+                    const endMarkRange = getMarkRange($to, editor.state.schema.marks.textDecoration);
+
+                    if (startMarkRange?.from && endMarkRange?.to) {
+                        // 处理链接选区
+                        const newSelection = TextSelection.create(
+                            editor.state.doc,
+                            startMarkRange.from,
+                            endMarkRange.to
+                        );
+
+                        editor.chain().setTextSelection(newSelection).run();
+                    }
 
                     commands.openAttrEditorPanel("textDecoration", textDecorationAttr, {
                         rect,
