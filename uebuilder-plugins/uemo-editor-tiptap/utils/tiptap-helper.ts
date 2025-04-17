ab3d28fdb407c608c3c1ecbb60151ef96fc0e96a
stone <877627290@qq.com>
2025-04-18 02:20:04 +0800

    update(uemo-editor-tiptap): 移除 getFontScaleExtensionStorage ，并使用 getFontScaleStorage 替换

--- uebuilder-plugins/uemo-editor-tiptap/utils/tiptap-helper.ts
+++ uebuilder-plugins/uemo-editor-tiptap/utils/tiptap-helper.ts
@@ -1,12 +1,3 @@
-import type { Editor } from "@tiptap/core";
-import type { fontScaleStorage } from "../packages/extension-font-scale";
-
-const getFontScaleExtensionStorage = (editor: Editor): fontScaleStorage | undefined => {
-    return editor.storage.fontScaleExtension;
-};
-
-export { getFontScaleExtensionStorage };
-
 const isAppleOS = () =>
     navigator.platform.includes("Mac") ||
     (navigator.userAgent.includes("AppleWebKit") && /Mobile\/\w+/.test(navigator.userAgent));
