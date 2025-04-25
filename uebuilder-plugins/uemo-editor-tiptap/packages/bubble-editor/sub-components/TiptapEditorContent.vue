b2e8a6a46312c8df834433de0c23727e51e4d1f6
stone <877627290@qq.com>
2025-04-25 11:58:04 +0800

    update(uemo-editor-tiptap): 添加AI扩展功能，包含AI处理器配置、加载状态管理及加载面板组件

--- uebuilder-plugins/uemo-editor-tiptap/packages/bubble-editor/sub-components/TiptapEditorContent.vue
+++ uebuilder-plugins/uemo-editor-tiptap/packages/bubble-editor/sub-components/TiptapEditorContent.vue
@@ -2,12 +2,14 @@
     <UeTiptapEditorMenu />
     <EditorContent :editor="editor" />
     <LinkPanel />
+    <AILoadingPanel />
 </template>
 <script lang="ts" setup>
 import type { Editor } from "@tiptap/vue-3";
 
 import { EditorContent } from "@tiptap/vue-3";
 import LinkPanel from "../../extension-link/panel/LinkPanel.vue";
+import AILoadingPanel from "../../extension-ai/panel/AILoadingPanel.vue";
 
 const _props = defineProps<{ editor: Editor }>();
 </script>
