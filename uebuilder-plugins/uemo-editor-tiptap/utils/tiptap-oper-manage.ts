74ef77d85f523b5490533f00b7f8fd04611a17ce
stone <877627290@qq.com>
2025-04-28 12:02:27 +0800

    update(uemo-editor-tiptap): 使用i18n更新操作管理中的文本，增强多语言支持

--- uebuilder-plugins/uemo-editor-tiptap/utils/tiptap-oper-manage.ts
+++ uebuilder-plugins/uemo-editor-tiptap/utils/tiptap-oper-manage.ts
@@ -1,3 +1,7 @@
+import { i18n } from "../src/i18n";
+
+const { t } = i18n.global;
+
 type OperInfo = {
     title: string;
     subtitle?: string;
@@ -10,37 +14,42 @@ type OperInfo = {
 const operManage = {
     // #region 文字相关操作
 
-    text: { tip: "插入文本", title: "文本", subtitle: "Text", icon: "icon-editor-font" },
-    bold: { title: "加粗", shortcut: "Mod+B", icon: "icon-editor-bold", buttonType: "iconButton" },
-    italic: { title: "斜体", shortcut: "Mod+I", icon: "icon-editor-italic", buttonType: "iconButton" },
-    textDecoration: { title: "文字装饰", icon: "icon-editor-svg-line", buttonType: "iconButton" },
-    blockquote: { title: "引用", shortcut: "Mod-Shift-B", icon: "icon-editor-quotes", buttonType: "iconButton" },
-    fontSize: { title: "字号", buttonType: "textButton" },
-    fontFamily: { title: "字体", buttonType: "textButton" },
-    textColor: { title: "文字颜色", buttonType: "colorButton" },
-    lineHeight: { title: "文字行高", icon: "icon-editor-line-height", buttonType: "iconButton" },
-    letterSpacing: { title: "字符间距", icon: "icon-editor-letter-spacing", buttonType: "iconButton" },
-    fontScale: { title: "字体缩放", icon: "icon-editor-font-scale", buttonType: "iconButton" },
+    text: { tip: t("TIP_INSERT_TEXT"), title: t("NODE_TEXT"), subtitle: t("NODE_TEXT_DESC"), icon: "icon-editor-font" },
+    bold: { title: t("OPER_BOLD"), shortcut: "Mod+B", icon: "icon-editor-bold", buttonType: "iconButton" },
+    italic: { title: t("OPER_ITALIC"), shortcut: "Mod+I", icon: "icon-editor-italic", buttonType: "iconButton" },
+    textDecoration: { title: t("OPER_TEXT_DECORATION"), icon: "icon-editor-svg-line", buttonType: "iconButton" },
+    blockquote: {
+        title: t("OPER_BLOCKQUOTE"),
+        shortcut: "Mod-Shift-B",
+        icon: "icon-editor-quotes",
+        buttonType: "iconButton",
+    },
+    fontSize: { title: t("OPER_FONT_SIZE"), buttonType: "textButton" },
+    fontFamily: { title: t("OPER_FONT_FAMILY"), buttonType: "textButton" },
+    textColor: { title: t("OPER_TEXT_COLOR"), buttonType: "colorButton" },
+    lineHeight: { title: t("OPER_LINE_HEIGHT"), icon: "icon-editor-line-height", buttonType: "iconButton" },
+    letterSpacing: { title: t("OPER_LETTER_SPACING"), icon: "icon-editor-letter-spacing", buttonType: "iconButton" },
+    fontScale: { title: t("OPER_FONT_SCALE"), icon: "icon-editor-font-scale", buttonType: "iconButton" },
 
     // #endregion
 
     // #region 文字对齐方式操作
 
-    textAlign: { title: "文字对齐方式", buttonType: "textButton" },
+    textAlign: { title: t("OPER_TEXT_ALIGN"), buttonType: "textButton" },
     textAlignLeft: {
-        title: "文字居左",
+        title: t("OPER_TEXT_ALIGN_LEFT"),
         shortcut: "Mod-Shift-L",
         icon: "icon-editor-text-left",
         buttonType: "iconButton",
     },
     textAlignCenter: {
-        title: "文字居中",
+        title: t("OPER_TEXT_ALIGN_CENTER"),
         shortcut: "Mod-Shift-E",
         icon: "icon-editor-text-center",
         buttonType: "iconButton",
     },
     textAlignRight: {
-        title: "文字居右",
+        title: t("OPER_TEXT_ALIGN_RIGHT"),
         shortcut: "Mod-Shift-R",
         icon: "icon-editor-text-right",
         buttonType: "iconButton",
@@ -50,65 +59,110 @@ const operManage = {
 
     // #region 媒体类型
 
-    insertSvgIcon: { title: "图标", subtitle: "Icon", tip: "插入图标", icon: "icon-editor-icon" },
+    insertSvgIcon: {
+        title: t("NODE_ICON"),
+        subtitle: t("NODE_ICON_DESC"),
+        tip: t("TIP_INSERT_ICON"),
+        icon: "icon-editor-icon",
+    },
     insertEmoji: {
-        tip: "插入 Emoji",
-        title: "表情符号",
-        subtitle: "Emoji",
+        tip: t("TIP_INSERT_EMOJI"),
+        title: t("NODE_EMOJI"),
+        subtitle: t("NODE_EMOJI_DESC"),
         icon: "icon-editor-emoji",
     },
-    insertSpline: { title: "Spline", subtitle: "3D 效果展示", tip: "插入 Spline", icon: "icon-editor-spline" },
-    insertLottie: { title: "Lottie", subtitle: "Lottie 动画展示", tip: "插入 Lottie", icon: "icon-editor-lottie" },
-    insertSvgViewer: { title: "SVG", subtitle: "SVG 展示", tip: "插入 SVG", icon: "icon-editor-svg" },
+    insertSpline: {
+        title: t("NODE_SPLINE"),
+        subtitle: t("NODE_SPLINE_DESC"),
+        tip: t("TIP_INSERT_SPLINE"),
+        icon: "icon-editor-spline",
+    },
+    insertLottie: {
+        title: t("NODE_LOTTIE"),
+        subtitle: t("NODE_LOTTIE_DESC"),
+        tip: t("TIP_INSERT_LOTTIE"),
+        icon: "icon-editor-lottie",
+    },
+    insertSvgViewer: {
+        title: t("NODE_SVG"),
+        subtitle: t("NODE_SVG_DESC"),
+        tip: t("TIP_INSERT_SVG"),
+        icon: "icon-editor-svg",
+    },
 
     // #endregion
 
     // #region 链接
 
-    link: { title: "链接", shortcut: "Mod-K", icon: "icon-editor-link", buttonType: "iconButton" },
-    insertButton: { title: "按钮", subtitle: "Button", tip: "插入按钮", icon: "icon-editor-button" },
+    link: { title: t("OPER_LINK"), shortcut: "Mod-K", icon: "icon-editor-link", buttonType: "iconButton" },
+    insertButton: {
+        title: t("NODE_BUTTON"),
+        subtitle: t("NODE_BUTTON_DESC"),
+        tip: t("TIP_INSERT_BUTTON"),
+        icon: "icon-editor-button",
+    },
 
     // #endregion
 
     // #region 社交
 
-    insertShare: { title: "社交", subtitle: "Social Contact", tip: "插入社交按钮", icon: "icon-editor-share" },
+    insertShare: {
+        title: t("NODE_SOCIAL"),
+        subtitle: t("NODE_SOCIAL_DESC"),
+        tip: t("TIP_INSERT_SHARE"),
+        icon: "icon-editor-share",
+    },
 
     // #endregion
 
     insertLoopText: {
-        title: "文字循环",
-        subtitle: "Loop Text",
-        tip: "插入文字循环",
+        title: t("NODE_LOOP_TEXT"),
+        subtitle: t("NODE_LOOP_TEXT_DESC"),
+        tip: t("TIP_INSERT_LOOP_TEXT"),
         icon: "icon-editor-loop-text",
     },
     insertEffectText: {
-        title: "特效文字",
-        subtitle: "Effect Text",
-        tip: "插入特效文字",
+        title: t("NODE_EFFECT_TEXT"),
+        subtitle: t("NODE_EFFECT_TEXT_DESC"),
+        tip: t("TIP_INSERT_EFFECT_TEXT"),
         icon: "icon-editor-effect-text",
     },
     insertCounterNumber: {
-        title: "计数器",
-        subtitle: "Count Up",
-        tip: "插入计数器",
+        title: t("NODE_COUNTER"),
+        subtitle: t("NODE_COUNTER_DESC"),
+        tip: t("TIP_INSERT_COUNTER"),
         icon: "icon-editor-counter-number",
     },
 
-    // #region frame
+    // #region 媒体类型
 
-    insertVideoFrame: { title: "视频", subtitle: "Video", tip: "嵌入视频", icon: "icon-editor-video" },
-    insertMapFrame: { title: "地图", subtitle: "Map", tip: "嵌入地图", icon: "icon-editor-map" },
-    insertWebFrame: { title: "网页", subtitle: "Web", tip: "嵌入网页", icon: "icon-editor-web" },
+    insertVideoFrame: {
+        title: t("NODE_VIDEO"),
+        subtitle: t("NODE_VIDEO_DESC"),
+        tip: t("TIP_INSERT_VIDEO"),
+        icon: "icon-editor-video",
+    },
+    insertMapFrame: {
+        title: t("NODE_MAP"),
+        subtitle: t("NODE_MAP_DESC"),
+        tip: t("TIP_INSERT_MAP"),
+        icon: "icon-editor-map",
+    },
+    insertWebFrame: {
+        title: t("NODE_WEB"),
+        subtitle: t("NODE_WEB_DESC"),
+        tip: t("TIP_INSERT_WEB"),
+        icon: "icon-editor-web",
+    },
 
     // #endregion
 
     // #region 结构类型
 
     insertDividerBlock: {
-        title: "分隔块",
-        subtitle: "Divider Block",
-        tip: "分隔块",
+        title: t("NODE_DIVIDER_BLOCK"),
+        subtitle: t("NODE_DIVIDER_BLOCK_DESC"),
+        tip: t("TIP_INSERT_DIVIDER_BLOCK"),
         icon: "icon-editor-divider-block",
     },
 
@@ -116,80 +170,124 @@ const operManage = {
 
     // #region 工具
 
-    editorAI: { title: "智能AI编辑工具", icon: "icon-editor-ai", buttonType: "iconButton" },
+    editorAI: { title: t("PLUGIN_AI_TOOL"), icon: "icon-editor-ai", buttonType: "iconButton" },
 
     // #endregion
 
     // #region 基础控件
 
-    add: { title: "增加按钮", icon: "icon-editor-add", buttonType: "iconButton" },
-    copy: { title: "复制", icon: "icon-editor-copy", buttonType: "iconButton" },
-    editor: { title: "编辑", icon: "icon-editor-image-setting", buttonType: "iconButton" },
-    replace: { title: "替换", buttonType: "textButton" },
-    deleteNode: { title: "移除", icon: "icon-editor-trash", buttonType: "iconButton" },
-    formatting: { title: "清除格式", icon: "icon-editor-remove-style", buttonType: "iconButton" },
-    delete: { title: "删除", icon: "icon-editor-trash" },
-    selectParent: { title: "选中上级", icon: "icon-editor-to-parent", buttonType: "iconButton" },
-    insertNewLineBefore: { title: "在前插入新行", icon: "icon-editor-hard-break-before", buttonType: "iconButton" },
-    insertNewLineAfter: { title: "在后插入新行", icon: "icon-editor-hard-break", buttonType: "iconButton" },
-    moreOper: { title: "更多", icon: "icon-editor-more", buttonType: "iconButton" },
-    hardBreak: { title: "添加换行", icon: "icon-editor-hard-break" },
-    removeSetting: { title: "清除当前设置", icon: "icon-editor-trash", buttonType: "iconButton" },
+    add: { title: t("OPER_ADD"), icon: "icon-editor-add", buttonType: "iconButton" },
+    copy: { title: t("OPER_COPY"), icon: "icon-editor-copy", buttonType: "iconButton" },
+    editor: { title: t("OPER_EDIT"), icon: "icon-editor-image-setting", buttonType: "iconButton" },
+    replace: { title: t("OPER_REPLACE"), buttonType: "textButton" },
+    deleteNode: { title: t("OPER_DELETE"), icon: "icon-editor-trash", buttonType: "iconButton" },
+    formatting: { title: t("OPER_FORMATTING"), icon: "icon-editor-remove-style", buttonType: "iconButton" },
+    delete: { title: t("OPER_DELETE"), icon: "icon-editor-trash" },
+    selectParent: { title: t("OPER_SELECT_PARENT"), icon: "icon-editor-to-parent", buttonType: "iconButton" },
+    insertNewLineBefore: {
+        title: t("OPER_INSERT_NEW_LINE_BEFORE"),
+        icon: "icon-editor-hard-break-before",
+        buttonType: "iconButton",
+    },
+    insertNewLineAfter: {
+        title: t("OPER_INSERT_NEW_LINE_AFTER"),
+        icon: "icon-editor-hard-break",
+        buttonType: "iconButton",
+    },
+    moreOper: { title: t("OPER_MORE"), icon: "icon-editor-more", buttonType: "iconButton" },
+    hardBreak: { title: t("OPER_HARD_BREAK"), icon: "icon-editor-hard-break" },
+    removeSetting: { title: t("OPER_REMOVE_SETTING"), icon: "icon-editor-trash", buttonType: "iconButton" },
 
     // #endregion
 
     // #region 表格相关
 
-    inertTable: { title: "表格", subtitle: "Table", tip: "插入表格", icon: "icon-editor-table" },
+    inertTable: {
+        title: t("NODE_TABLE"),
+        subtitle: t("NODE_TABLE_DESC"),
+        tip: t("TIP_INSERT_TABLE"),
+        icon: "icon-editor-table",
+    },
 
     // 表格操作
-    toggleTableBorder: { title: "切换边框显示", buttonType: "iconButton" },
-    selectTable: { title: "选中表格", icon: "icon-editor-select-table", buttonType: "iconButton" },
-    removeTable: { title: "移除表格", icon: "icon-editor-trash", buttonType: "iconButton" },
-    tableBorderColor: { title: "边框颜色", buttonType: "colorButton" },
+    toggleTableBorder: { title: t("OPER_TOGGLE_TABLE_BORDER"), buttonType: "iconButton" },
+    selectTable: { title: t("OPER_SELECT_TABLE"), icon: "icon-editor-select-table", buttonType: "iconButton" },
+    removeTable: { title: t("OPER_REMOVE_TABLE"), icon: "icon-editor-trash", buttonType: "iconButton" },
+    tableBorderColor: { title: t("OPER_TABLE_BORDER_COLOR"), buttonType: "colorButton" },
 
     // 单元格操作
-    tableScale: { title: "表格扩充", icon: "icon-editor-table-scale", buttonType: "iconButton" },
-    tableWidth: { title: "单元格宽度", icon: "icon-editor-width", buttonType: "iconButton" },
-    tableMinWidth: { title: "单元格最小宽度", icon: "icon-editor-min-width", buttonType: "iconButton" },
-    tableCellBg: { title: "单元格背景色", buttonType: "colorButton" },
-    tableCellAlign: { title: "单元格内容对齐方式", buttonType: "iconButton" },
-    mergeTableCell: { title: "合并/分割 单元格", icon: "icon-editor-cell-merge", buttonType: "iconButton" },
-    resetColWidth: { title: "清空单元格宽度设置", icon: "icon-editor-remove-style", buttonType: "iconButton" },
+    tableScale: { title: t("OPER_TABLE_SCALE"), icon: "icon-editor-table-scale", buttonType: "iconButton" },
+    tableWidth: { title: t("OPER_TABLE_WIDTH"), icon: "icon-editor-width", buttonType: "iconButton" },
+    tableMinWidth: { title: t("OPER_TABLE_MIN_WIDTH"), icon: "icon-editor-min-width", buttonType: "iconButton" },
+    tableCellBg: { title: t("OPER_TABLE_CELL_BG"), buttonType: "colorButton" },
+    tableCellAlign: { title: t("OPER_TABLE_CELL_ALIGN"), buttonType: "iconButton" },
+    mergeTableCell: { title: t("OPER_MERGE_TABLE_CELL"), icon: "icon-editor-cell-merge", buttonType: "iconButton" },
+    resetColWidth: { title: t("OPER_RESET_COL_WIDTH"), icon: "icon-editor-remove-style", buttonType: "iconButton" },
 
     // 行操作
-    selectRow: { title: "选中当前行", icon: "icon-editor-table-row", buttonType: "iconButton" },
-    deleteRow: { title: "移除当前行", icon: "icon-editor-trash", buttonType: "iconButton" },
-    addRowBefore: { title: "向上插入行", icon: "icon-editor-table-insert-row-before", buttonType: "iconButton" },
-    addRowAfter: { title: "向下插入行", icon: "icon-editor-table-insert-row-after", buttonType: "iconButton" },
+    selectRow: { title: t("OPER_SELECT_ROW"), icon: "icon-editor-table-row", buttonType: "iconButton" },
+    deleteRow: { title: t("OPER_DELETE_ROW"), icon: "icon-editor-trash", buttonType: "iconButton" },
+    addRowBefore: {
+        title: t("OPER_ADD_ROW_BEFORE"),
+        icon: "icon-editor-table-insert-row-before",
+        buttonType: "iconButton",
+    },
+    addRowAfter: {
+        title: t("OPER_ADD_ROW_AFTER"),
+        icon: "icon-editor-table-insert-row-after",
+        buttonType: "iconButton",
+    },
 
     // 列操作
-    selectCol: { title: "选中当前列", icon: "icon-editor-table-col", buttonType: "iconButton" },
-    deleteColumn: { title: "移除当前列", icon: "icon-editor-trash", buttonType: "iconButton" },
-    addColumnBefore: { title: "向左插入列", icon: "icon-editor-table-insert-col-before", buttonType: "iconButton" },
-    addColumnAfter: { title: "向右插入列", icon: "icon-editor-table-insert-col-after", buttonType: "iconButton" },
+    selectCol: { title: t("OPER_SELECT_COL"), icon: "icon-editor-table-col", buttonType: "iconButton" },
+    deleteColumn: { title: t("OPER_DELETE_COLUMN"), icon: "icon-editor-trash", buttonType: "iconButton" },
+    addColumnBefore: {
+        title: t("OPER_ADD_COLUMN_BEFORE"),
+        icon: "icon-editor-table-insert-col-before",
+        buttonType: "iconButton",
+    },
+    addColumnAfter: {
+        title: t("OPER_ADD_COLUMN_AFTER"),
+        icon: "icon-editor-table-insert-col-after",
+        buttonType: "iconButton",
+    },
 
     // #endregion
 
     // #region 分隔线
 
-    hrRule: { title: "分隔线", subtitle: "Divider Line", tip: "分隔线", icon: "icon-editor-divider-solid" },
-    toggleHrStyle: { title: "切换分隔线样式", buttonType: "iconButton" },
-    hrColor: { title: "分隔线颜色", buttonType: "colorButton" },
+    hrRule: {
+        title: t("NODE_HR"),
+        subtitle: t("NODE_HR_DESC"),
+        tip: t("TIP_INSERT_HR"),
+        icon: "icon-editor-divider-solid",
+    },
+    toggleHrStyle: { title: t("OPER_TOGGLE_HR_STYLE"), buttonType: "iconButton" },
+    hrColor: { title: t("OPER_HR_COLOR"), buttonType: "colorButton" },
 
     // #endregion
 
     // #region 网格相关
 
-    inertGridGroup: { title: "网格", subtitle: "Grid Group", tip: "插入网格", icon: "icon-editor-grid-layer" },
-    selectGridGroup: { title: "选中网格排布", icon: "icon-editor-to-parent", buttonType: "iconButton" },
+    inertGridGroup: {
+        title: t("NODE_GRID"),
+        subtitle: t("NODE_GRID_DESC"),
+        tip: t("TIP_INSERT_GRID"),
+        icon: "icon-editor-grid-layer",
+    },
+    selectGridGroup: { title: t("OPER_SELECT_GRID_GROUP"), icon: "icon-editor-to-parent", buttonType: "iconButton" },
 
     // #endregion
 
     // #region 图片相关
 
-    addImage: { title: "图片", subtitle: "Image", tip: "插入图片", icon: "icon-editor-image-add" },
-    replaceImage: { title: "更换图片", icon: "icon-editor-repace-image", buttonType: "iconButton" },
+    addImage: {
+        title: t("NODE_IMAGE"),
+        subtitle: t("NODE_IMAGE_DESC"),
+        tip: t("TIP_INSERT_IMAGE"),
+        icon: "icon-editor-image-add",
+    },
+    replaceImage: { title: t("OPER_REPLACE_IMAGE"), icon: "icon-editor-repace-image", buttonType: "iconButton" },
 
     // #endregion
 
