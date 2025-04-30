6e57f8e0998476ac1a66377fb0baa8cfe1e3febb
stone <877627290@qq.com>
2025-04-30 10:36:02 +0800

    update(uemo-editor-tiptap): 添加收集文本样式的功能，支持字体和字号的动态更新

--- uebuilder-plugins/uemo-editor-tiptap/packages/bubble-editor/sub-components/TiptapEditorContent.vue
+++ uebuilder-plugins/uemo-editor-tiptap/packages/bubble-editor/sub-components/TiptapEditorContent.vue
@@ -13,7 +13,43 @@ import LinkPanel from "../../extension-link/panel/LinkPanel.vue";
 import AILoadingPanel from "../../extension-ai/panel/AILoadingPanel.vue";
 import AddNodePanel from "../../extension-node-placeholder/panel/AddNodePanel.vue";
 
-const _props = defineProps<{ editor: Editor }>();
+const props = defineProps<{ editor: Editor }>();
+
+// 定义收集字体和字号的方法
+const collectTextStyles = () => {
+    const fontFamilyList: string[] = [];
+    const fontSizeList: string[] = ["default"];
+
+    // 遍历文档中的所有文本节点
+    props.editor?.state.doc.descendants((node) => {
+        if (!node.isText) return;
+
+        // 检查节点的文本样式标记
+        node.marks.forEach((mark) => {
+            if (mark.type.name !== "textStyle") return;
+
+            const { fontFamily, fontSize } = mark.attrs;
+
+            // 收集字体
+            if (fontFamily && !fontFamilyList.includes(fontFamily)) {
+                fontFamilyList.push(fontFamily);
+            }
+
+            // 收集字号
+            if (fontSize && !fontSizeList.includes(fontSize)) {
+                fontSizeList.push(fontSize);
+            }
+        });
+    });
+
+    // 输出收集到的字体列表
+    // console.log("fontFamilyList", fontFamilyList);
+};
+
+onMounted(() => {
+    // 监听编辑器更新事件
+    props.editor.on("update", collectTextStyles);
+});
 </script>
 <style lang="scss" module>
 //
