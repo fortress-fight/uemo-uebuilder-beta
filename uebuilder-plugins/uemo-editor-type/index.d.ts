768e9e421f1fa2b48c47635e333e43e74ec08429
stone <877627290@qq.com>
2025-05-06 13:55:12 +0800

    update(uemo-editor-type): 新增全局类型定义文件并更新索引文件引用

--- uebuilder-plugins/uemo-editor-type/index.d.ts
+++ uebuilder-plugins/uemo-editor-type/index.d.ts
@@ -1,6 +1,7 @@
 /*
  * @Description: uemo-template-editor 全局类型
  * @Author: F-Stone
- * @LastEditTime: 2025-04-12 16:20:49
+ * @LastEditTime: 2025-04-30 13:20:45
  */
 /// <reference types="./utils.d.ts" />
+/// <reference types="./global.d.ts" />
