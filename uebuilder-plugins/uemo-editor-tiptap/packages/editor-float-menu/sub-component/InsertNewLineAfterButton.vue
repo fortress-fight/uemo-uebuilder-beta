bd2b47bed902784dff43b51683ba043be69331f5
stone <877627290@qq.com>
2025-05-09 00:30:48 +0800

    update(uemo-editor-tiptap): 优化插入新行功能，直接通过链式调用插入新行，移除旧的插入方法

--- uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/InsertNewLineAfterButton.vue
+++ uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/InsertNewLineAfterButton.vue
@@ -2,14 +2,14 @@
     <UeTiptapMenuButton type="insertNewLineAfter" @trigger="trigger" />
 </template>
 <script lang="ts" setup>
-import { insertNewLine } from "../../../utils/tiptap-utils";
 import { useInjectTiptapEditor } from "../../../utils/mixin-tiptap-editor";
 
 const { editor } = useInjectTiptapEditor();
 
 function trigger() {
     if (!editor) return false;
-    insertNewLine(editor, "after");
+
+    editor.chain().insertNewLine("after").run();
 }
 </script>
 <style lang="scss" module>
--- uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/InsertNewLineBeforeButton.vue
+++ uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/InsertNewLineBeforeButton.vue
@@ -2,14 +2,14 @@
     <UeTiptapMenuButton type="insertNewLineBefore" @trigger="trigger" />
 </template>
 <script lang="ts" setup>
-import { insertNewLine } from "../../../utils/tiptap-utils";
 import { useInjectTiptapEditor } from "../../../utils/mixin-tiptap-editor";
 
 const { editor } = useInjectTiptapEditor();
 
 function trigger() {
     if (!editor) return false;
-    insertNewLine(editor, "before");
+
+    editor.chain().insertNewLine("before").run();
 }
 </script>
 <style lang="scss" module>
