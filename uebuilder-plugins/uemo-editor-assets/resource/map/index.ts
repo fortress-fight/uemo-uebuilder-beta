b6b1029e22b4e55ae478a3f3e4a131397235b069
stone <877627290@qq.com>
2025-06-07 13:05:32 +0800

    update(uemo-editor-assets): 新增地图资源类型定义及默认地图库配置

---
+++ uebuilder-plugins/uemo-editor-assets/resource/map/index.ts
@@ -0,0 +1,8 @@
+export type ResourceMap = { type: "gaode"; mapUrl: string };
+
+const mapLib: ResourceMap = {
+    type: "gaode",
+    mapUrl: "https://static.jsmo.xin/uebuilder/v4/plugins/map/gaode-map.html",
+};
+
+export default mapLib;
