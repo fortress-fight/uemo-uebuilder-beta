96eddf2830c710b95059f32763fb2cf82725a9de
stone <877627290@qq.com>
2025-06-29 18:46:21 +0800

    update(uemo-editor-tiptap)：优化选择父节点功能，新增对网格单元的处理逻辑

--- uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/SelectParentButton.vue
+++ uebuilder-plugins/uemo-editor-tiptap/packages/editor-float-menu/sub-component/SelectParentButton.vue
@@ -2,13 +2,30 @@
     <UeTiptapMenuButton type="selectParent" @trigger="trigger" />
 </template>
 <script lang="ts" setup>
+import { selectNode } from "../../../utils/tiptap-utils";
+import { getClosestGridGroup } from "../../extension-grid/utils/helper";
 import { useInjectTiptapEditor } from "../../../utils/mixin-tiptap-editor";
 
 const { editor } = useInjectTiptapEditor();
+const props = defineProps<{ nodeName?: string }>();
 
 function trigger() {
     if (!editor) return false;
-    editor.chain().selectParentNode().focus().run();
+    switch (props.nodeName) {
+        case "gridItem":
+            const gridGroupInfo = getClosestGridGroup(editor);
+            if (!gridGroupInfo) return;
+
+            const { pos } = gridGroupInfo;
+            selectNode(editor, pos);
+
+            editor.chain().focus().run();
+            break;
+
+        default:
+            editor.chain().selectParentNode().focus().run();
+            break;
+    }
 }
 </script>
 <style lang="scss" module>
