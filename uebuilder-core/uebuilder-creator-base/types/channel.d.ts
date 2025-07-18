d5799575405c0a96ece378ca0072af6917ffe1b5
stone <877627290@qq.com>
2025-07-18 15:09:32 +0800

    feature(uebuilder-core): 在 Creator 工作台通道中新增 CREATOR_STOREHOUSE_CHANNEL 类型定义，支持测试方法的跨窗口通信

--- uebuilder-core/uebuilder-creator-base/types/channel.d.ts
+++ uebuilder-core/uebuilder-creator-base/types/channel.d.ts
@@ -1,11 +1,18 @@
 /*
  * @Description: 定义 Creator 能够调用的 Workbench 方法
  * @Author: F-Stone
- * @LastEditTime: 2025-07-18 14:03:14
+ * @LastEditTime: 2025-07-18 15:07:03
  */
 import type { Methods } from "@stone/uemo-editor-utils/lib/penpal/message-channel";
 
 export namespace CREATOR_WORKBENCH_CHANNEL {
+    export interface Api extends Methods {
+        // 测试方法
+        test: () => Promise<string>;
+    }
+}
+
+export namespace CREATOR_STOREHOUSE_CHANNEL {
     export interface Api extends Methods {
         // 测试方法
         test: () => Promise<string>;
