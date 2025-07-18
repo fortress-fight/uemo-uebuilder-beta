9de5e2475f70fb9f2b6c68bde794bffa89cf02c9
stone <877627290@qq.com>
2025-07-18 13:27:22 +0800

    feature(uemo-editor-utils): 新增消息通道模块，包含 MessageChannel 类实现，支持跨窗口通信

---
+++ uebuilder-plugins/uemo-editor-utils/lib/penpal/message-channel.ts
@@ -0,0 +1,58 @@
+/*
+ * @Description: uebuilder-creator 到 uebuilder-workbench 消息通道
+ * @Author: F-Stone
+ * @LastEditTime: 2025-07-18 11:34:49
+ */
+
+import type { Connection, Methods, RemoteProxy } from "@stone/uemo-editor-utils/lib/penpal";
+import { WindowMessenger, connect, debug } from "@stone/uemo-editor-utils/lib/penpal";
+
+export type { Methods };
+
+interface MessageChannelApi extends Methods {
+    _getInfo: () => { from: string; to: string };
+}
+
+export class MessageChannel<T extends Methods> {
+    private readonly messenger: WindowMessenger;
+    private readonly connection: Connection<T & MessageChannelApi>;
+
+    constructor(
+        private readonly remoteWindow: Window,
+        options: {
+            from: string;
+            to: string;
+            origin: string;
+            methods: Methods;
+            log?: boolean;
+        }
+    ) {
+        if (!remoteWindow) {
+            throw new Error("window is null");
+        }
+
+        this.messenger = new WindowMessenger({
+            remoteWindow: remoteWindow,
+            allowedOrigins: [options.origin],
+        });
+
+        const channelName = `${options.from} ==> ${options.to}`;
+        this.connection = connect<T & MessageChannelApi>({
+            channel: channelName,
+            messenger: this.messenger,
+            log: options.log ? debug(channelName) : undefined,
+            methods: {
+                ...options.methods,
+                _getInfo: () => ({ from: options.from, to: options.to }),
+            },
+        });
+    }
+
+    get remote(): Promise<RemoteProxy<T & MessageChannelApi>> {
+        return this.connection.promise;
+    }
+
+    destroy(): void {
+        this.connection.destroy();
+    }
+}
