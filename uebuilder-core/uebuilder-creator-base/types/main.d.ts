c2ada4f764f2efbdc3e86ac9b5906c2352eebede
stone <877627290@qq.com>
2025-07-19 15:59:30 +0800

    update(uebuilder-core): 更新依赖项，新增 uebuilder-type、uemo-editor-type 和 uebuilder-workbench-base，扩展类型定义以支持更灵活的模块配置

--- uebuilder-core/uebuilder-creator-base/types/main.d.ts
+++ uebuilder-core/uebuilder-creator-base/types/main.d.ts
@@ -1,21 +1,78 @@
+/// <reference types="@stone/uebuilder-type/global.d.ts" />
+
+type TYPE_CUSTOM_MODULE_ATTRS = Record<
+    `c-${string}`,
+    | { type: "text"; name: string; default?: string }
+    | {
+          type: "select";
+          name: string;
+          options: { value: string; text: string }[];
+          default?: string;
+          button?: { type: "link"; text: string; value: Record<string, string> }[];
+      }
+>;
+
 declare global {
-    namespace UE_BUILDER {
-        /**
-         * initial：初始模式
-         * editing：编辑模式
-         * preview：预览模式
-         * template：模板库模式
-         * replace： 替换编辑页面模式
-         * building： 极速构建模式
-         */
-        type State = "editing" | "initial" | "preview" | "template" | "replace" | "building";
-    }
     namespace UE_BUILDER_CREATOR {
         type InitParams = {
-            // 初始化时是否全屏
-            initFullSize: boolean;
+            // 应用名称
+            appName: "UEMO_ADMIN" | "JSMO" | "MO005" | "CUSTOM" | "UEMO_TOOLS" | "APP_TEST";
+
             // 应用主路径
             appPath: string;
+
+            // 应用状态
+            appState?: Omit<UE_BUILDER.State, "template" | "replace">;
+
+            // 应用背景
+            appBackground?: string;
+
+            // 应用插件
+            appPlugin?: {
+                upload: UE_BUILDER.UploadConfig;
+                // 用于编辑时插入 js 和 css 内容
+                editor?: { name: string; type: "css" | "js"; link: string }[];
+            };
+
+            // 初始化时是否全屏
+            initFullSize?: boolean;
+
+            // 数据预处理
+            pageDataPreprocessing?: { type: "replaceString"; rule: string; data: string }[];
+
+            // 保存数据预处理
+            savePagePreprocessing?: { type: "replaceString"; rule: string; data: string }[];
+
+            // 自定义模块参数
+            customModule?: {
+                blockModule?: {
+                    modules: (
+                        | {
+                              title: string;
+                              type: string;
+                              thumbnail: string;
+                              enhance?: boolean;
+                              description?: string;
+                              attrs?: TYPE_CUSTOM_MODULE_ATTRS;
+                          }
+                        | {
+                              title: string;
+                              type: string;
+                              enhance?: boolean;
+                              description?: string;
+                              variants: {
+                                  id: string;
+                                  name: string;
+                                  thumbnail: string;
+                                  attrs: TYPE_CUSTOM_MODULE_ATTRS;
+                              }[];
+                          }
+                    )[];
+                };
+            };
+
+            // 获取页面数据
+            getPageData?: () => Promise<string>;
         };
     }
 }
--- uebuilder-core/uebuilder-workbench-base/types/main.d.ts
+++ uebuilder-core/uebuilder-workbench-base/types/main.d.ts
@@ -1,5 +1,11 @@
+/// <reference types="@stone/uebuilder-type/global.d.ts" />
+
 declare global {
     namespace UE_BUILDER_WORKBENCH {
+        type Config = {
+            version: string;
+        };
+
         type InitParams = {
             // 初始化时是否全屏
             initFullSize: boolean;
