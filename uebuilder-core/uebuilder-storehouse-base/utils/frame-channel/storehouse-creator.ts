bf094a17cbca73ddd5727167371378581f57f621
stone <877627290@qq.com>
2025-07-19 16:00:12 +0800

    update(uebuilder-core):  uebuilder-storehouse 优化 StorehouseCreatorChannel 类，新增 localApi 属性以支持更灵活的 API 定义

--- uebuilder-core/uebuilder-storehouse-base/utils/frame-channel/storehouse-creator.ts
+++ uebuilder-core/uebuilder-storehouse-base/utils/frame-channel/storehouse-creator.ts
@@ -1,27 +1,30 @@
 /*
  * @Description: uebuilder-creator 到 uebuilder-workbench 消息通道
  * @Author: F-Stone
- * @LastEditTime: 2025-07-18 15:08:31
+ * @LastEditTime: 2025-07-18 16:54:26
  */
 import type { CREATOR_STOREHOUSE_CHANNEL } from "@stone/uebuilder-creator-base/types/channel";
 import type { STOREHOUSE_CREATOR_CHANNEL } from "../../types/channel";
 
 import { MessageChannel } from "@stone/uemo-editor-utils/lib/penpal/message-channel";
 
-const storehouseCreatorApi: STOREHOUSE_CREATOR_CHANNEL.Api = {
-    test: () => Promise.resolve("storehouseCreatorApi"),
-};
-
-export class StorehouseCreatorChannel extends MessageChannel<CREATOR_STOREHOUSE_CHANNEL.Api> {
+export class StorehouseCreatorChannel extends MessageChannel<
+    CREATOR_STOREHOUSE_CHANNEL.Api,
+    STOREHOUSE_CREATOR_CHANNEL.Api
+> {
     private static instance: StorehouseCreatorChannel | null = null;
+
+    readonly localApi = {
+        test: () => Promise.resolve("storehouseCreatorApi"),
+    };
 
     private constructor(remoteWindow: Window) {
         super(remoteWindow, "creatorStorehouse", {
             from: "storehouse",
             to: "creator",
             origin: "*",
-            methods: storehouseCreatorApi,
         });
+        this.connect();
     }
 
     public static getInstance(remoteWindow: Window) {
