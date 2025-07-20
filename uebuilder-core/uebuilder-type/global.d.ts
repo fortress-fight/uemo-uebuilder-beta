0278adf284825ea82e351a32cb4729d8247f7305
stone <877627290@qq.com>
2025-07-20 17:10:04 +0800

    update(uebuilder-type): 更新 UploadConfig 类型定义，引用外部类型并移除冗余配置

--- uebuilder-core/uebuilder-type/global.d.ts
+++ uebuilder-core/uebuilder-type/global.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="@stone/uemo-editor-element" />
+
 declare global {
     namespace UE_BUILDER {
         /**
@@ -13,60 +15,7 @@ declare global {
         /**
          * 上传配置
          */
-        type UploadConfig = {
-            uploadPath: string;
-            uploadName: string;
-            publicPath: string;
-            resourceLink: string;
-            fileLimitSize: number;
-            withCredentials?: boolean;
-            useFullLink?: boolean;
-            uploadData?: { [key: string]: string };
-            uploadFileQueryPath: string;
-            history?: { type: "MO005" } | { type: "custom"; url: ""; addResourceLink?: true };
-
-            qiniu?:
-                | {
-                      withCredentials?: boolean;
-                      // acceptType?: "*" | ("mp4" | "image" | "other")[];
-
-                      tokenUrl: string;
-                      tokenPath: string;
-                      tokenRegionPath: string;
-                      tokenFolderPath: string;
-                      tokenUrlPrefix?: string;
-
-                      uploadRegion: string;
-                      uploadFilePath: string;
-                      uploadLimitSize?: number;
-                      UploadConfig?: Record<string, any>;
-                      uploadCustomVars?: Record<string, string>;
-                  }
-                | false;
-
-            image?: {
-                // 是否允许上传视频，默认：true
-                allow: boolean;
-                // 上传文件大小限制，默认：2048
-                limitSize?: number;
-                errorMsg?: {
-                    // 未开通时的提示信息，默认："未开通图片上传功能，请联系客服询问详情",
-                    notAllow?: string;
-                };
-            };
-
-            video?: {
-                // 是否允许上传视频，默认：false
-                allow: boolean;
-                // 上传文件大小限制，默认：2048
-                limitSize?: number;
-                useQiniu?: boolean;
-                errorMsg?: {
-                    // 未开通时的提示信息，默认："未开通视频上传功能，请联系客服询问详情",
-                    notAllow?: string;
-                };
-            };
-        };
+        type UploadConfig = UE_EL_UTIL.UploadConfig;
     }
 }
 
