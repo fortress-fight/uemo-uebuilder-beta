e095319253c6b4b5a1aabf1949e55e1c55f616f3
stone <877627290@qq.com>
2025-07-20 17:07:49 +0800

    update(uemo-editor-element): toast-plugin 修改 createToast 函数以简化插件安装过程，更新最后编辑时间

--- uebuilder-plugins/uemo-editor-element/packages/toast-plugin/index.ts
+++ uebuilder-plugins/uemo-editor-element/packages/toast-plugin/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Description: toast 插件
  * @Author: F-Stone
- * @LastEditTime: 2025-03-03 12:49:15
+ * @LastEditTime: 2025-07-20 16:44:21
  */
 import type { App } from "vue";
 import type { PluginOptions } from "@stone/uemo-editor-utils/lib/vue-toastification";
@@ -30,12 +30,16 @@ const defaultParam: PluginOptions = {
     newestOnTop: true,
 };
 
+export function createToast(param: PluginOptions = {}) {
+    return createToastInterface({ ...defaultParam, ...param });
+}
+
 /**
  * Vue3 插件安装方法，将全局属性挂载到 app.config.globalProperties
  * @param {App} app - Vue3 应用实例
  * @param {PluginOptions} param - 插件配置参数
  */
 export function install(app: App, param: PluginOptions = {}) {
-    const toast = createToastInterface({ ...defaultParam, ...param });
+    const toast = createToast(param);
     app.config.globalProperties.$ueElToast = toast;
 }
