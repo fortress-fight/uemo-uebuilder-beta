b14027f49dc0f8ccd701162f895ca05b39b255fc
stone <877627290@qq.com>
2025-07-21 02:22:17 +0800

    update(uebuilder-workbench-base): 在 tsconfig.json 中添加 store 目录的类型引用，并在 main.d.ts 中恢复 uemo-editor-type 的引用，增加工作台状态类型定义

--- uebuilder-core/uebuilder-workbench-base/types/main.d.ts
+++ uebuilder-core/uebuilder-workbench-base/types/main.d.ts
@@ -1,10 +1,12 @@
-/// <reference types="@stone/uebuilder-type/global.d.ts" />
+/// <reference types="@stone/uebuilder-type" />
+/// <reference types="@stone/uemo-editor-type" />
 /// <reference types="@stone/uebuilder-creator-base" />
 
 declare global {
     namespace UE_BUILDER_WORKBENCH {
         type Config = {
             version: string;
+            workbenchState: UE_BUILDER.State;
         };
 
         type InitParams = {
