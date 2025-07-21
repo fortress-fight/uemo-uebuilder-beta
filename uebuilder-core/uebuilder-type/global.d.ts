0e4cbde901d8725a3ad5406696619f598bb0c086
stone <877627290@qq.com>
2025-07-21 15:09:39 +0800

    update(uebuilder-type): 更新 global.d.ts 文件，新增应用名称和应用类型的类型定义

--- uebuilder-core/uebuilder-type/global.d.ts
+++ uebuilder-core/uebuilder-type/global.d.ts
@@ -3,14 +3,29 @@
 declare global {
     namespace UE_BUILDER {
         /**
+         * 应用名称
+         * @description 应用名称，包括 UEMO_ADMIN、JSMO、MO005、CUSTOM、UEMO_TOOLS、APP_TEST
+         */
+        type Name = "UEMO_ADMIN" | "JSMO" | "MO005" | "CUSTOM" | "UEMO_TOOLS" | "APP_TEST";
+
+        /**
+         * 应用类型
+         * @description 应用类型，包括布局和页面
+         *  - layout: 布局
+         *  - page: 页面
+         */
+        type Type = "layout" | "page";
+
+        /**
          * 工作台状态
          * @description 工作台状态，包括初始、编辑、预览、模板库、替换、极速构建
-         *  - initial：初始模式 -- 展示工作台
+         *  - entry: 进入工作台前的“即将编辑”页面展示
+         *  - browsing: 工作台开启后的预览/库展示状态（原 initial）
          *  - editing：编辑模式 -- 编辑页面
          *  - preview：预览模式 -- 预览页面
-         *  - building： 极速构建模式 -- 极速构建页面
+         *  - composer 极速构建模式 -- 极速构建页面
          */
-        type State = "initial" | "editing" | "preview" | "building";
+        type State = "entry" | "browsing" | "editing" | "preview" | "composer";
 
         /**
          * 上传配置
