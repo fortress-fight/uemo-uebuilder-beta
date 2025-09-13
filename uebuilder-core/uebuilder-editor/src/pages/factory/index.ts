cf8dce9c8862301e5342387bc09e39f7a6d21703
stone <877627290@qq.com>
2025-09-14 02:16:00 +0800

    update(uebuilder-editor): 新增 Factory 控制面板页面及编辑器工厂通道实现，重构相关类以支持消息传递功能

--- uebuilder-core/uebuilder-editor/src/pages/factory/index.ts
+++ uebuilder-core/uebuilder-editor/src/pages/factory/index.ts
@@ -1,21 +1,71 @@
-import pk from "~/package.json";
+/*
+ * @Description: UEBuilder 编辑层 -- 控制面板页面
+ * @Author: F-Stone
+ * @LastEditTime: 2025-09-14 02:15:05
+ */
+import { EditorFactoryWorkbenchChannel } from "./utils/frame-channel";
 
-window.name = "UEBUILDER_STARTER";
+import $ from "@stone/uemo-editor-utils/lib/jquery";
 
-export const VERSION = "v" + pk.version;
-export const pageEditorMap = new Map<string, UEBUILDER_STARTER_BASE>();
+window.name = "UEBUILDER_EDITOR_FACTORY";
 
-export abstract class UEBUILDER_STARTER_BASE {
-    constructor(public dom: HTMLElement, public option: UE_BUILDER_STARTER.Config) {
-        //
+/**
+ * UeBuilderFactory 编辑器控制面板基类
+ * @class UebuilderEditorFactoryBase
+ * @description 负责管理和创建 UeBuilderEditorFactory 编辑器控制面板实例
+ */
+export abstract class UebuilderEditorFactoryBase {
+    /** 初始化状态标志 */
+    public initialized = false;
+
+    public StorehouseWorkbenchChannelCreator = EditorFactoryWorkbenchChannel;
+
+    /**
+     * 构造函数
+     * @param {HTMLElement} rootDom - 根 DOM 元素
+     */
+    constructor(public rootDom: HTMLElement) {
+        // eslint-disable-next-line
+        console.log("rootDom", rootDom);
+    }
+
+    /**
+     * 初始化消息通道
+     * @private
+     */
+    public editorFactoryWorkbenchChannel: EditorFactoryWorkbenchChannel | null = null;
+    async initializeMessageChannel(): Promise<void> {
+        this.editorFactoryWorkbenchChannel = this.StorehouseWorkbenchChannelCreator.getInstance(window.parent, {
+            on: {
+                launchEditorFactory: (config) => {
+                    return this.launchEditorFactory(config);
+                },
+            },
+        });
+
+        const remote = await this.editorFactoryWorkbenchChannel.remote;
+        await remote.editorFactoryReady();
+
+        // const info = await remote._getInfo();
+        // console.log("Workbench => Editor", info);
     }
 
     /**
      * 初始化 app-main 插入 app-main 的 frame
      */
     init() {
+        if (this.initialized) {
+            return this;
+        }
+
+        this.initializeMessageChannel().catch((err) => {
+            console.error(err);
+        });
+
         return this;
     }
+
+    abstract launchEditorFactory(config: UE_BUILDER_EDITOR_FACTORY.Config): Promise<void>;
 
     /**
      * 销毁 App
@@ -25,4 +75,26 @@ export abstract class UEBUILDER_STARTER_BASE {
     }
 }
 
-export default { UEBUILDER_STARTER: UEBUILDER_STARTER_BASE };
+class UeBuilderEditorFactory extends UebuilderEditorFactoryBase {
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+    constructor(dom: HTMLElement) {
+        super(dom);
+    }
+
+    launchEditorFactory(config: UE_BUILDER_EDITOR_FACTORY.Config) {
+        // eslint-disable-next-line
+        console.log("config", config);
+        return Promise.resolve();
+    }
+}
+
+export function createUeBuilderEditorFactory(dom: HTMLElement) {
+    return new UeBuilderEditorFactory(dom);
+}
+
+$(() => {
+    const container = document.querySelector<HTMLElement>("#UEBUILDER-EDITOR-FACTORY");
+    if (!container) return;
+
+    createUeBuilderEditorFactory(container).init();
+});
---
+++ uebuilder-core/uebuilder-editor/src/pages/factory/utils/frame-channel/editor-factory-workbench.ts
@@ -0,0 +1,65 @@
+/*
+ * @Description: uebuilder-editor 发送到 uebuilder-workbench 消息通道
+ * @Author: F-Stone
+ * @LastEditTime: 2025-09-14 00:40:23
+ */
+import type { WORKBENCH_EDITOR_FACTORY_CHANNEL } from "@stone/uebuilder-workbench-base/types/channel";
+import type { EDITOR_FACTORY_WORKBENCH_CHANNEL } from "../../../../../types/channel";
+
+import { MessageChannel } from "@stone/uemo-editor-utils/lib/penpal/message-channel";
+
+type Param = {
+    on: {
+        launchEditorFactory: (config: UE_BUILDER_EDITOR_FACTORY.Config) => Promise<void>;
+    };
+};
+
+export class EditorFactoryWorkbenchChannel extends MessageChannel<
+    WORKBENCH_EDITOR_FACTORY_CHANNEL.Api,
+    EDITOR_FACTORY_WORKBENCH_CHANNEL.Api
+> {
+    get localApi(): EDITOR_FACTORY_WORKBENCH_CHANNEL.Api {
+        return {
+            launchEditorFactory: (config) => {
+                return this.param.on.launchEditorFactory(config);
+            },
+        } as EDITOR_FACTORY_WORKBENCH_CHANNEL.Api;
+    }
+
+    constructor(
+        readonly remoteWindow: Window,
+        private readonly param: Param
+    ) {
+        super(remoteWindow, "workbenchEditorFactoryChannel", {
+            from: "editorFactory",
+            to: "workbench",
+            origin: "*",
+        });
+        this.connect();
+    }
+
+    public static getInstance(remoteWindow: Window, param: Param) {
+        if (!remoteWindow) {
+            throw new Error("Invalid remote window");
+        }
+
+        const channelManager = this.channelManager;
+        if (!channelManager.has(remoteWindow)) {
+            // NOTE
+            // 使用 new this 而不是 new EditorFactoryWorkbenchChannel 是因为 this 是使用当前类的子类
+            channelManager.set(remoteWindow, new this(remoteWindow, param));
+        }
+
+        const instance = channelManager.get(remoteWindow) as EditorFactoryWorkbenchChannel;
+
+        if (!instance) {
+            throw new Error("EditorFactoryWorkbenchChannel instance is not found");
+        }
+        return instance;
+    }
+
+    destroy() {
+        super.destroy();
+        EditorFactoryWorkbenchChannel.channelManager.delete(this.remoteWindow);
+    }
+}
