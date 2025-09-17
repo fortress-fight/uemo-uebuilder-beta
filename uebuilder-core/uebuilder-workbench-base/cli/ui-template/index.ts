83c4a8ecdd2778f058057732ec9ac615433365cd
stone <877627290@qq.com>
2025-09-17 12:17:20 +0800

    update(uebuilder-workbench-base): 新增组件创建 CLI 工具，支持根据模板生成 Vue 组件文件及类型定义

---
+++ uebuilder-core/uebuilder-workbench-base/cli/ui-template/index.ts
@@ -0,0 +1,20 @@
+/*
+* @Description: <%- description %>
+* @Author: F-Stone
+ * @LastEditTime: 2025-07-26 17:37:10
+*/
+import type { App } from "vue";
+
+import <%- componentName %> from "./Main.vue";
+
+<%- componentName %>.install = (app: App) => {
+    if (!<%- componentName %>.name) return;
+    app.component(<%- componentName %>.name, <%- componentName %>);
+};
+
+export interface <%- componentName %>BaseProps {
+    disabled?: boolean;
+}
+export type <%- componentName %>Instance = InstanceType<typeof <%- componentName %>>;
+
+export default <%- componentName %>;
